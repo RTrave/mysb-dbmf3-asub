826a9ae1250a5dcd9160c36a293701582e891aef
Roman Trave <roman.trave@abadcafe.org>
2026-06-15 10:53:10 +0200

    upd: bump to version 3a

--- __init.php
+++ __init.php
@@ -17,7 +17,7 @@ class MySBModule_dbmf3_asub extends MySBModuleHelper
 
     public $lname = 'dbmf3_asub';
     public $version = 3;
-    public $release_version = '2b';
+    public $release_version = '3a';
     public $homelink = 'https://github.com/RTrave/mysb-dbmf3-asub';
     public $require = array(
         'core' => 7,
